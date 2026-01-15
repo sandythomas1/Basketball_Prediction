33298b805d30feeea5037ac41d2f453c1ebe63be
Sandy Thomas <sandy.thomas@calbaptist.edu>
2026-01-14 23:39:59 -0800

    profile page work and actions hopeful fixes

--- app/lib/Screens/profile_screen.dart
+++ app/lib/Screens/profile_screen.dart
@@ -1,20 +1,69 @@
 // basic place holder for profile screen
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../theme/app_theme.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'login_screen.dart';
 
 class ProfileScreen extends ConsumerWidget {
   const ProfileScreen({super.key});
+
+  // show profile details(email, profile pic later)
+  // edit button to change profile details (implement logic later)
+  // logout button(get working)
+
+  Future<void> _logOut(BuildContext context) async {
+    try {
+      await FirebaseAuth.instance.signOut();
+      if (context.mounted) {
+        Navigator.of(context).pushReplacementNamed('/login');
+      }
+    } catch (e) {
+      if (context.mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Error logging out: $e')),
+        );
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Profile', style: TextStyle(color: context.textPrimary)),
-        backgroundColor: context.bgSecondary,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: context.textPrimary),
-          onPressed: () => Navigator.pop(context),
+        title: const Text('Profile'),
+        //backgroundColor: AppTheme.appBarColor,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('assets/profile_placeholder.png'),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'User Email',
+              style: TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Implement edit profile logic
+              },
+              child: const Text('Edit Profile'),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                // log out logic
+                _logOut(context);
+              },
+              child: const Text('Logout'),
+            ),
+          ],
         ),
       ),
     );
