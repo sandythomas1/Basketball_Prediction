7a55df88b33aed64a850020b3cde7755171b3638
sandythomas1 <sandy.thomas@calbaptist.edu>
2026-01-23 21:21:34 -0800

    test

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'Widgets/auth_gate.dart';
 import 'theme/app_theme.dart';
+// test
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
